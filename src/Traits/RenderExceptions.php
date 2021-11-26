d9744f75f38510486b8ef35309062004edb0a010
Saul Chelewani <saul@patkay.net>
2021-11-26 14:06:04 +0200

    resolve merge conflicts

--- src/Traits/RenderExceptions.php
+++ src/Traits/RenderExceptions.php
@@ -8,7 +8,7 @@ use Bluecloud\ResponseBuilder\ResponseBuilder;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
@@ -21,7 +21,7 @@ trait RenderExceptions
         if ($exception instanceof ModelNotFoundException || $exception instanceof NotFoundHttpException)
             return (new ResponseBuilder())->notFound()->build();
 
-        $message = config('app.debug') ? $exception->getMessage() : 'Error while processing your request';
+        $message = config('app.debug') ? $exception->getMessage() : $failureMessage;
         $builder = (new ResponseBuilder())->failed($message);
 
         if (config('app.debug')) $builder->trace($exception->getTrace());
